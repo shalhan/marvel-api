21a2e8a51d63a0e8a94843d6885e99109bbc0698
shalhan.radifan <shalhan.radifan@online-pajak.com>
2021-07-17 00:34:01 +0700

    move marvel api url to env file

--- src/Common/services/MarvelIntegrationService.ts
+++ src/Common/services/MarvelIntegrationService.ts
@@ -10,9 +10,9 @@ export class MarvelIntegrationService implements ApiGet<MarvelApiResponse> {
   private timestamp: string;
   private publicKey: string;
   constructor(private readonly httpService: HttpService) {
-    const privateKey = process.env.MARVEL_PRIVATE_KEY;
+    const privateKey = process.env.MARVEL_API_PRIVATE_KEY;
     this.timestamp = `${new Date().getTime()}`;
-    this.publicKey = process.env.MARVEL_PUBLIC_KEY;
+    this.publicKey = process.env.MARVEL_API_PUBLIC_KEY;
     this.hash = MD5(`${this.timestamp}${privateKey}${this.publicKey}`);
   }
   private async callRequest(
@@ -22,7 +22,7 @@ export class MarvelIntegrationService implements ApiGet<MarvelApiResponse> {
     const queryParam = `?ts=${this.timestamp}&apikey=${this.publicKey}`;
     const response = await this.httpService
       .request<MarvelApiResponse>({
-        baseURL: 'http://gateway.marvel.com/v1/public/',
+        baseURL: process.env.MARVEL_API_URL,
         method: method as any,
         url: `${endpoint}${queryParam}`,
       })
